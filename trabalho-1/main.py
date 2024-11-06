b89c224a0316c226f11ff253d2ca4cc97030785d
Emanuelle Guse <emanuguse@gmail.com>
2024-11-06 14:50:42 -0300

    arruma codigo

--- trabalho-1/main.py
+++ trabalho-1/main.py
@@ -1,71 +1,100 @@
 from termcolor import colored
 import utils
 
-colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan',
+# Lista de cores para os blocos do tabuleiro
+COLORS = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan',
           'light_grey', 'dark_grey', 'light_red', 'light_green', 
           'light_yellow', 'light_blue', 'light_magenta', 'light_cyan']
 
-tamanho = utils.board1().get_tamanho()
-blocks = utils.board1().get_blocks()
-board = utils.board1().get_board()
+# Inicialização do tabuleiro, blocos e tamanho
+def initialize_game():
+    board_instance = utils.board1()
+    return board_instance.get_tamanho(), board_instance.get_blocks(), board_instance.get_board()
 
-def print_board(): # funciona
+TAMANHO, BLOCKS, BOARD = initialize_game()
+
+#####################################
+######## Funções de Get/Set #########
+#####################################
+
+def get_board_position(row, col):
+    return BOARD[row][col]
+
+def set_board_position(row, col, value):
+    BOARD[row][col] = value
+
+def get_block_position(row, col):
+    return BLOCKS[row][col]
+
+def get_region_size(row, col):
+    region_id = get_block_position(row, col)
+    return sum(1 for i in range(TAMANHO) for j in range(TAMANHO) if get_block_position(i, j) == region_id)
+
+#####################################
+######## Funções de Exibição ########
+#####################################
+
+def get_block_color(row, col):
+    """Retorna a cor correspondente ao ID do bloco na posição (row, col)."""
+    block_id = get_block_position(row, col) - 1
+    return COLORS[block_id % len(COLORS)]
+
+def print_element(cell_value, color):
+    print(colored(cell_value, color), end=('' if cell_value == '██' else ' '))
+
+def print_board():
     """Imprime o tabuleiro com cores de acordo com os blocos."""
-    for i in range(tamanho):
-        for j in range(tamanho):
+    for i in range(TAMANHO):
+        for j in range(TAMANHO):
             color = get_block_color(i, j)
-            cell_value = board[i][j]
-            print(colored(cell_value, color), end=('' if cell_value == '██' else ' '))
+            cell_value = get_board_position(i, j)
+            print_element(cell_value, color)
         print()
     print()
 
-def get_block_color(row, col): # funciona
-    """Retorna a cor correspondente ao ID do bloco na posição (row, col)."""
-    block_id = blocks[row][col] - 1
-    return colors[block_id % len(colors)]
+#####################################
+######## Funções de Validação #######
+#####################################
 
-def get_region_size(row, col):
-    region_id = blocks[row][col]
-    return sum(1 for i in range(tamanho) for j in range(tamanho) if blocks[i][j] == region_id)
+def same_region(region_id, num):
+    """Verifica se o número já existe na mesma região."""
+    return any(get_block_position(i, j) == region_id and get_board_position(i, j) == num
+               for i in range(TAMANHO) for j in range(TAMANHO))
+
+def ortogonally_valid(row, col, num):
+    """Verifica a validade ortogonal da posição."""
+    for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        r, c = row + dr, col + dc
+        if 0 <= r < TAMANHO and 0 <= c < TAMANHO and get_board_position(r, c) == num:
+            return False
+    return True
+
+def vertically_valid(row, col, num):
+    """Verifica a validade vertical da posição."""
+    if row > 0 and get_board_position(row - 1, col) == num:
+        return False
+    if row < TAMANHO - 1 and get_board_position(row + 1, col) == num:
+        return False
+    return True
 
 def is_valid(row, col, num):
     """Verifica se um número pode ser colocado na célula (row, col) sem violar as regras."""
-    region_id = blocks[row][col]
+    region_id = get_block_position(row, col)
+    return not same_region(region_id, num) and ortogonally_valid(row, col, num) and vertically_valid(row, col, num)
 
-    # Verificar se o número já está na mesma região
-    for i in range(tamanho):
-        for j in range(tamanho):
-            if blocks[i][j] == region_id and (board[i][j]) == num:
-                return False
-
-    # Verificar células adjacentes ortogonalmente
-    for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-        r, c = row + dr, col + dc
-        if 0 <= r < tamanho and 0 <= c < tamanho and board[r][c] == num:
-            return False
-
-    # Verificar a regra de ordem vertical para a mesma região
-    if row > 0 and blocks[row][col] == blocks[row - 1][col]:
-        if board[row - 1][col] != '██' and (board[row - 1][col]) <= num:
-            return False
-    if row < tamanho - 1 and blocks[row][col] == blocks[row + 1][col]:
-        if board[row + 1][col] != '██' and (board[row + 1][col]) >= num:
-            return False
-
-    return True
-
+#####################################
+######## Funções de Solução #########
+#####################################
 
 def get_candidate_cells():
-    """Ordena as células vazias para serem preenchidas primeiro as que estão em regiões menores e com mais restrições."""
-    empty_cells = [(i, j) for i in range(tamanho) for j in range(tamanho) if board[i][j] == '██']
+    """Ordena as células vazias para priorizar as de regiões menores e com mais restrições."""
+    empty_cells = [(i, j) for i in range(TAMANHO) for j in range(TAMANHO) if get_board_position(i, j) == '██']
     return sorted(empty_cells, key=lambda cell: (get_region_size(cell[0], cell[1]), len(get_valid_numbers(cell[0], cell[1]))))
-
 
 def get_valid_numbers(row, col):
     """Retorna uma lista de números válidos para uma célula específica."""
     region_size = get_region_size(row, col)
     return [num for num in range(1, region_size + 1) if is_valid(row, col, num)]
-
 
 def solve():
     """Resolve o quebra-cabeça usando backtracking otimizado."""
@@ -75,13 +104,20 @@ def solve():
 
     row, col = candidate_cells[0]
     for num in get_valid_numbers(row, col):
-        board[row][col] = num
+        set_board_position(row, col, num)
         if solve():
             return True
-        board[row][col] = '██' 
+        set_board_position(row, col, '██')
 
     return False
-    
+
+#####################################
+############ Execução ###############
+#####################################
+
 print_board()
-solve()
+if solve():
+    print("Solução encontrada:")
+else:
+    print("Não foi possível encontrar uma solução.")
 print_board()
