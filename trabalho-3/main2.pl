dc8cbcb3dbf332b9b32bf90cfd65c863aa849c7d
Emanuelle Guse <manuguse@manuguse.labsec.ufsc.br>
2024-12-11 15:32:40 -0300

    funcioan acho

--- trabalho-3/main2.pl
+++ trabalho-3/main2.pl
@@ -1,195 +1,188 @@
-%-- Matrizes de Entrada ------------------------------------------------------------------------------------------------------
-
-/* Módulos utilizados na solução */
 :- use_module(library(apply)).
 :- use_module(library(lists)).
 
-/* Matriz 6x6 que contém todos os números iniciais do puzzle kojun, sendo 0 uma posição vazia */
-matrizNumerosInicial([
-    [2,0,0,0,1,0],
-    [0,0,0,3,0,0],
-    [0,3,0,0,5,3],
-    [0,0,0,0,0,0],
-    [0,0,3,0,4,2],
-    [0,0,0,0,0,0]
-]).
+puzzle(
+    0,
+    [
+        [0, 2, 1],
+        [2, 1, 0],
+        [1, 0, 2]
+    ],
+    [
+        [1, 1, 2],
+        [3, 4, 4],
+        [3, 3, 4]
+    ]
+).
 
-/* Matriz 6x6 que define as regiões do quebra-cabeça. Essas regiões devem ser representadas por inteiros de 0 até n. */
-/* Sendo n a quantidade de regiões - 1, esses inteiros podem ser considerados o id de cada região. */
-matrizRegioes([
-    [0 ,0 ,1 ,1 ,1 ,2],
-    [3 ,3 ,3 ,3 ,3 ,2],
-    [4 ,5 ,5 ,5 ,3 ,6],
-    [4 ,4 ,4 ,5 ,6 ,6],
-    [7 ,7 ,8 ,9 ,9 ,9],
-    [10,10,8 ,8 ,9 ,9]
-]).
+puzzle(
+    1,
+    [
+        [2, 0, 0, 0, 1, 0],
+        [0, 0, 3, 0, 0, 0],
+        [0, 3, 0, 0, 5, 3],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 3, 0, 4, 2],
+        [0, 0, 0, 0, 0, 0]
+    ],
+    [
+        [1, 1, 7, 7, 7, 11],
+        [2, 2, 2, 2, 2, 11],
+        [3, 6, 6, 6, 2, 10],
+        [3, 3, 3, 6, 10, 10],
+        [4, 4, 8, 9, 9, 9],
+        [5, 5, 8, 8, 9, 9]
+    ]
+).
 
-%-- Operações em Matrizes ----------------------------------------------------------------------------------------------------
+puzzle(
+    8,
+    [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 3, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 3, 0, 0],
+        [0, 0, 3, 0, 0, 0, 0, 0],
+        [0, 5, 0, 3, 0, 0, 0, 0],
+        [0, 2, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 3, 0],
+        [0, 0, 5, 3, 0, 0, 0, 0]
+    ],
+    [
+        [1, 1, 9, 9, 11, 13, 16, 16],
+        [1, 1, 2, 9, 12, 13, 13, 16],
+        [2, 2, 2, 7, 12, 14, 17, 17],
+        [3, 3, 3, 7, 12, 14, 14, 17],
+        [4, 7, 7, 7, 7, 14, 14, 17],
+        [4, 5, 10, 10, 10, 15, 18, 17],
+        [5, 5, 5, 5, 8, 15, 15, 15],
+        [6, 8, 8, 8, 8, 15, 19, 19]
+    ]
+).
 
-/* Usando recursividade para imprimir cada elemento de uma lista */
-imprimirLista([]).
-imprimirLista([H|T]) :- write(H), write(" "), imprimirLista(T).
 
-/* Usando recursividade para imprimir cada linha de uma matriz */
-imprimirMatriz([]).
-imprimirMatriz([H|T]) :- imprimirLista(H), nl, imprimirMatriz(T).
+board(Id, Board) :- puzzle(Id, Board, _).
+blocks(Id, Blocks) :- puzzle(Id, _, Blocks).
 
-/* A operação "nth0()" pode retornar o enésimo valor de uma lista */
-/* Como as matrizes utilizadas nessa solução são listas de listas, "buscarMatriz()" nos retorna o valor desejado */
-buscarMatriz(Matriz, Linha, Coluna, Valor) :- nth0(Linha, Matriz, Lista), nth0(Coluna, Lista, Valor).
+print_line([]).
+print_line([H|T]) :- write(H), write(" "), print_line(T).
 
-/* Percorre a lista até Posicao - 1 (length), atualiza o valor e armazena em Lista2 */
-atualizarPosicao(Posicao, ValorAntigo, ValorNovo, Lista1, Lista2) :- 
+print_board([]).
+print_board([H|T]) :- print_line(H), nl, print_board(T).
+
+get_cell(Board, Row, Column, CellValue) :- nth0(Row, Board, Lista), nth0(Column, Lista, CellValue).
+
+update_cell(Posicao, ValorAntigo, ValorNovo, Lista1, Lista2) :- 
     length(Pos, Posicao),
     append(Pos, [ValorAntigo|Resto], Lista1),
     append(Pos, [ValorNovo|Resto], Lista2).
 
-/* Faz o mesmo que a função anterior, mas em uma lista de listas */
-atualizarMatriz(Matriz, I, J, ValorNovo, NovaMatriz) :-
-    atualizarPosicao(I, Antigo, Novo, Matriz, NovaMatriz),
-    atualizarPosicao(J, _Antigo, ValorNovo, Antigo, Novo).
+update_board(Board, I, J, ValorNovo, NewBoard) :-
+    update_cell(I, Antigo, Novo, Board, NewBoard),
+    update_cell(J, _Antigo, ValorNovo, Antigo, Novo).
 
-%-- Listas -------------------------------------------------------------------------------------------------------------------
+block_number(BoardID, Board, IdRegiao, CellValue) :-
+    blocks(BoardID, MatrizRegioes),
+    get_cell(MatrizRegioes, I, J, IdRegiao),
+    get_cell(Board, I, J, CellValue).
 
-/* Retornando todos os números presentes em uma região com base em um ID de entrada */
-buscarNumerosRegiao(Matriz, IdRegiao, Valor) :-
-    matrizRegioes(MatrizRegioes),
-    buscarMatriz(MatrizRegioes, I, J, IdRegiao),
-    buscarMatriz(Matriz, I, J, Valor).
+block_numbers(BoardID, Board, IdRegiao, ListaNumerosRegiao) :- 
+    findall(CellValue, block_number(BoardID, Board, IdRegiao, CellValue), ListaNumerosRegiao).
 
-/* Utilizando "findall()" para gerar uma lista que contenha todos os números de uma determinada região */
-listaNumerosRegiao(Matriz, IdRegiao, ListaNumerosRegiao) :- 
-    findall(Valor, buscarNumerosRegiao(Matriz, IdRegiao, Valor), ListaNumerosRegiao).
-
-/* Retornando uma lista com os números que completam uma região */
-/* Exemplo: seja Lista = [1, 0, 5, 2, 0], o retorno será [3, 4] */
-/* Em uma região completa, o retorno é [] */
-listaComplemento(Lista, ListaComplemento) :-
-    length(Lista, Tamanho),
-    numlist(1, Tamanho, ListaTotal),
+missing_numbers(Lista, ListaComplemento) :-
+    length(Lista, Size),
+    numlist(1, Size, ListaTotal),
     delete(Lista, 0, ListaResto),
     subtract(ListaTotal, ListaResto, ListaComplemento).
 
-/* Retornando as coordenadas de uma região que possuem o valor 0 na matriz de números */
-buscarCoordenadasLivres(Matriz, IdRegiao, [I, J]) :-
-    matrizRegioes(MatrizRegioes),
-    buscarMatriz(MatrizRegioes, I, J, IdRegiao),
-    buscarMatriz(Matriz, I, J, 0).
+empty_cell(BoardID, Board, IdRegiao, [I, J]) :-
+    blocks(BoardID, MatrizRegioes),
+    get_cell(MatrizRegioes, I, J, IdRegiao),
+    get_cell(Board, I, J, 0).
 
-/* Utilizando "findall()" para gerar uma lista que contenha todas as coordenadas livres de uma determinada região */
-listaCoordenadasLivres(Matriz, IdRegiao, ListaCoordenadasLivres) :- 
-    findall([I, J], buscarCoordenadasLivres(Matriz, IdRegiao, [I, J]), ListaCoordenadasLivres).
+empty_cells(BoardID, Board, IdRegiao, ListaCoordenadasLivres) :- 
+    findall([I, J], empty_cell(BoardID, Board, IdRegiao, [I, J]), ListaCoordenadasLivres).
 
-%-- Verificações -------------------------------------------------------------------------------------------------------------
-
-/* Verificando se a coordenada de entrada já possui um valor ou não com base em "listaCoordenadasLivres()" */
-verificarCoordenadas(Matriz, I, J) :-
-    matrizRegioes(MatrizRegioes),
-    buscarMatriz(MatrizRegioes, I, J, IdRegiao),
-    listaCoordenadasLivres(Matriz, IdRegiao, ListaCoordenadasLivres),
+verificarCoordenadas(BoardID, Board, I, J) :-
+    blocks(BoardID, MatrizRegioes),
+    get_cell(MatrizRegioes, I, J, IdRegiao),
+    empty_cells(BoardID, Board, IdRegiao, ListaCoordenadasLivres),
     member([I, J], ListaCoordenadasLivres).
 
-/* Retorna um valor que não esteja na região utilizando "listaComplemento()" */
-verificarMembro(Matriz, I, J, Valor) :-
-    matrizRegioes(MatrizRegioes),
-    buscarMatriz(MatrizRegioes, I, J, IdRegiao),
-    listaNumerosRegiao(Matriz, IdRegiao, ListaNumerosRegiao),
-    listaComplemento(ListaNumerosRegiao, ListaValoresFaltantes),
-    member(Valor, ListaValoresFaltantes).
+check_cell(BoardID, Board, I, J, CellValue) :-
+    blocks(BoardID, MatrizRegioes),
+    get_cell(MatrizRegioes, I, J, IdRegiao),
+    block_numbers(BoardID, Board, IdRegiao, ListaNumerosRegiao),
+    missing_numbers(ListaNumerosRegiao, ListaValoresFaltantes),
+    member(CellValue, ListaValoresFaltantes).
 
-/* Faz parte de "verificarMaiorRegiao()" */
-/* Zero não é considerado maior quando está acima de um número na matriz */
 verificarValor(Valor1, Valor2) :-
     Valor2 =:= 0 -> true; Valor1 < Valor2.
 
-/* Retorna um valor que é menor que o número da posição acima dele na matriz */
-/* Primeiramente é avaliado se as posições pertencem à mesma região para depois comparar os valores */
-verificarMaiorRegiao(Matriz, I, J, Valor, Cima) :-
-    matrizRegioes(MatrizRegioes),
-    buscarMatriz(MatrizRegioes, I, J, IdRegiao),
-    buscarMatriz(MatrizRegioes, Cima, J, IdRegiaoCima),
+verificarMaiorRegiao(BoardID, Board, I, J, CellValue, Upper) :-
+    blocks(BoardID, MatrizRegioes),
+    get_cell(MatrizRegioes, I, J, IdRegiao),
+    get_cell(MatrizRegioes, Upper, J, IdRegiaoCima),
     IdRegiao =:= IdRegiaoCima ->
-    buscarMatriz(Matriz, Cima, J, ValorCima),
-    verificarValor(Valor, ValorCima);
-    buscarMatriz(Matriz, Cima, J, ValorCima),
-    Valor =\= ValorCima.
+    get_cell(Board, Upper, J, ValorCima),
+    verificarValor(CellValue, ValorCima);
+    get_cell(Board, Upper, J, ValorCima),
+    CellValue =\= ValorCima.
 
-/* Retorna um valor que é maior que o número da posição abaixo dele na matriz */
-/* Primeiramente é avaliado se as posições pertencem à mesma região para depois comparar os valores */
-verificarMenorRegiao(Matriz, I, J, Valor, Baixo) :-
-    matrizRegioes(MatrizRegioes),
-    buscarMatriz(MatrizRegioes, I, J, IdRegiao),
-    buscarMatriz(MatrizRegioes, Baixo, J, IdRegiaoBaixo),
+verificarMenorRegiao(BoardID, Board, I, J, CellValue, Lower) :-
+    blocks(BoardID, MatrizRegioes),
+    get_cell(MatrizRegioes, I, J, IdRegiao),
+    get_cell(MatrizRegioes, Lower, J, IdRegiaoBaixo),
     IdRegiao =:= IdRegiaoBaixo ->
-    buscarMatriz(Matriz, Baixo, J, ValorBaixo), Valor > ValorBaixo; 
-    buscarMatriz(Matriz, Baixo, J, ValorBaixo), Valor =\= ValorBaixo.
+    get_cell(Board, Lower, J, LowerCellValue), CellValue > LowerCellValue; 
+    get_cell(Board, Lower, J, LowerCellValue), CellValue =\= LowerCellValue.
 
-/* Dada uma coordenada, verifica se a posição acima dela na matriz é valida */
-/* Se a posição de cima for válida, retorna um valor que seja menor que o número dessa posição */
-verificarCima(Matriz, I, J, Valor) :-
-    Cima is (I - 1), Cima >= 0 ->
-    verificarMaiorRegiao(Matriz, I, J, Valor, Cima); true.
+verify_upper(BoardID, Board, I, J, CellValue) :-
+    Upper is (I - 1), Upper >= 0 ->
+    verificarMaiorRegiao(BoardID, Board, I, J, CellValue, Upper); true.
 
-/* Dada uma coordenada, verifica se a posição abaixo dela na matriz é valida */
-/* Se a posição de baixo for válida, retorna um valor que seja maior que o número dessa posição */
-verificarBaixo(Matriz, I, J, Valor) :-
-    length(Matriz, Tamanho),
-    Baixo is (I + 1), Baixo < Tamanho ->
-    verificarMenorRegiao(Matriz, I, J, Valor, Baixo); true.
+verify_lower(BoardID, Board, I, J, CellValue) :-
+    length(Board, Size),
+    Lower is (I + 1), Lower < Size ->
+    verificarMenorRegiao(BoardID, Board, I, J, CellValue, Lower); true.
 
-/* Dada uma coordenada, retorna um valor que seja diferente do número à sua esquerda na matriz */
-verificarEsquerda(Matriz, I, J, Valor) :-
-    Esquerda is (J - 1), Esquerda >= 0 ->
-    buscarMatriz(Matriz, I, Esquerda, ValorEsquerda),
-    Valor =\= ValorEsquerda; true.
+verify_left(Board, I, J, CellValue) :-
+    Left is (J - 1), Left >= 0 ->
+    get_cell(Board, I, Left, LeftCellValue),
+    CellValue =\= LeftCellValue; true.
 
-/* Dada uma coordenada, retorna um valor que seja diferente do número à sua direita na matriz */
-verificarDireita(Matriz, I, J, Valor) :- 
-    length(Matriz, Tamanho),
-    Direita is (J + 1), Direita < Tamanho ->
-    buscarMatriz(Matriz, I, Direita, ValorDireita),
-    Valor =\= ValorDireita; true.
+verify_right(Board, I, J, CellValue) :- 
+    length(Board, Size),
+    Right is (J + 1), Right < Size ->
+    get_cell(Board, I, Right, RightCellValue),
+    CellValue =\= RightCellValue; true.
 
-%-- Preenchendo a Matriz -----------------------------------------------------------------------------------------------------
+fill_position(BoardID, Board, I, J, NewBoard) :-
+    verificarCoordenadas(BoardID, Board, I, J) ->
+    check_cell(BoardID, Board, I, J, CellValue),
+    verify_upper(BoardID, Board, I, J, CellValue),
+    verify_lower(BoardID, Board, I, J, CellValue),
+    verify_left(Board, I, J, CellValue),
+    verify_right(Board, I, J, CellValue),
+    update_board(Board, I, J, CellValue, NewBoard);
+    NewBoard = Board.
 
-/* Dado uma matriz e uma coordenada, retorna um valor válido para essa posição segundo as regras do jogo */
-/* Com o valor válido, gera uma nova matriz de números e a retorna */
-/* Caso a posição já esteja preenchida, retorna a própria matriz de entrada */
-preencherPosicao(Matriz, I, J, NovaMatriz) :-
-    verificarCoordenadas(Matriz, I, J) ->
-    verificarMembro(Matriz, I, J, Valor),
-    verificarCima(Matriz, I, J, Valor),
-    verificarBaixo(Matriz, I, J, Valor),
-    verificarEsquerda(Matriz, I, J, Valor),
-    verificarDireita(Matriz, I, J, Valor),
-    atualizarMatriz(Matriz, I, J, Valor, NovaMatriz);
-    NovaMatriz = Matriz.
-
-%-- Resolvendo o Puzzle ------------------------------------------------------------------------------------------------------
-
-/* Aplicando "preencherPosicao()" para cada posição da matriz */
-/* Para cada posição, é dada uma matriz de entrada e uma nova matriz é retornada */
-% Função para preencher a matriz recursivamente
-preencherMatriz(Matriz, Linha, Coluna, NovaMatriz) :-
-    length(Matriz, Tamanho),
-    ( Linha < Tamanho, Coluna < Tamanho ->
-        preencherPosicao(Matriz, Linha, Coluna, MatrizAtualizada),
-        ProximaColuna is Coluna + 1,
-        ( ProximaColuna < Tamanho ->
-            preencherMatriz(MatrizAtualizada, Linha, ProximaColuna, NovaMatriz)
-        ; ProximaLinha is Linha + 1,
-          preencherMatriz(MatrizAtualizada, ProximaLinha, 0, NovaMatriz)
+fill_board(BoardID, Board, Row, Column, NewBoard) :-
+    length(Board, Size),
+    ( Row < Size, Column < Size ->
+        fill_position(BoardID, Board, Row, Column, UpdatedBoard),
+        NextColumn is Column + 1,
+        ( NextColumn < Size ->
+            fill_board(BoardID, UpdatedBoard, Row, NextColumn, NewBoard)
+        ; NextRow is Row + 1,
+          fill_board(BoardID, UpdatedBoard, NextRow, 0, NewBoard)
         )
-    ; NovaMatriz = Matriz
+    ; NewBoard = Board
     ).
 
-% Chamada inicial para preencher a matriz
-preencherMatrizInicial(MatrizInicial, MatrizFinal) :-
-    preencherMatriz(MatrizInicial, 0, 0, MatrizFinal).
+solve(BoardID, MatrizInicial, MatrizFinal) :-
+    fill_board(BoardID, MatrizInicial, 0, 0, MatrizFinal).
 
-% Exemplo de uso
-kojun :-
-    matrizNumerosInicial(MatrizInicial), % Inicialize sua matriz aqui
-    preencherMatrizInicial(MatrizInicial, MatrizFinal),
-    nl, imprimirMatriz(MatrizFinal), nl.
+kojun(BoardID) :-
+    board(BoardID, MatrizInicial), 
+    solve(BoardID, MatrizInicial, MatrizFinal),
+    nl, print_board(MatrizFinal), nl.
